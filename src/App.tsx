7e9e8b181219e72ff7b2fab90114ec7e3ec26f06
Tilek M <vvv.tilek@gmail.com>
2020-11-03 21:26:36 +0600

    Revert "Homework 3 - ESlint"
    This reverts commit 7e52c3f8b214356128fa0ce5358883df7126d44e.

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,15 @@ import cn from 'classnames';
 
 import s from './App.module.scss';
 // import './custom.css';
-import HeaderBlock from './components/HeaderBlock';
-import BodyBlock from './components/BodyBlock';
-import FooterBlock from './components/FooterBlock';
-
-
-// <div className={cn(s.header, 'color')}>
-// 	This is App component!
-// </div>
-// )
-
 
 const App = () => {
 	console.log('####: Some log');
 
 	return (
-		<React.Fragment>
-	      <HeaderBlock title="Keep in Touch with React" descr="Chaos philosophy ocean zarathustra hope mountains virtues decrepit inexpedient insofar strong depths ocean insofar. Disgust virtues eternal-return law enlightenment sea disgust grandeur evil ubermensch eternal-return christian war. Transvaluation ocean suicide god merciful. Pinnacle noble revaluation spirit reason intentions selfish christian. Ascetic merciful pinnacle joy god truth of gains victorious convictions."/>
-	      <BodyBlock />
-	      <FooterBlock />
-	    </React.Fragment>
-    );
+		<div className={cn(s.header, 'color')}>
+			This is App component!
+		</div>
+		)
 };
 
 export default App;
